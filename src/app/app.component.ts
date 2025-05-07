0b326f088deacac675e166cc44af2f1672b19d4e
Prathameshgargote <patyagargote@gmail.com>
2025-05-07 12:18:18 +0530

    second_commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tdform1';
+  userObj:Array<any>=[]
+
+  contactmsg=[
+    {
+      mode:'Phome',
+      id:'1'
+    },
+    {
+      mode:'Email',
+      id:'2'
+    }
+  ]
+
+  onSubmit(signup:NgForm){
+    console.log(signup);  
+  if(signup.valid){
+    let obj=signup.value
+    this.userObj.push(obj)
+    signup.resetForm()
+  }
+  }
 }
